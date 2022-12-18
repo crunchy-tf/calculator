440852ed30f39c5339b5c92c04e6673ba07bc7ec
Ayoub <ayoubdhaouadi08@gmail.com>
2022-12-18 04:07:55 +0100

    Made the string maker event listener

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-eval_operation = () => {
-division = document.querySelector('')
-}
+//make string (include DELETE)
+//evaluate string with = 
+//reset with AC
+//active display on screen (add at the end to every function)
 
-operate = (num1,num2,operation) => {
+btns = document.querySelectorAll('.button')
+btnsArr = Array.from(btns)
+btnsArr.splice(0,1) //remove AC
+btnsArr.splice(15,1) //remove =
 
-}
+clearBtn = document.querySelector('#clear')
+
+equalsBtn = document.querySelector('#equals')
+
+str = ''
+
+btnsArr.forEach(element => {
+    element.addEventListener('click',() => {
+        if (element.innerText == '÷') {
+            str+='/'
+        } else if (element.innerText == 'x') {
+            str+='*'
+        } else if (element.innerText == 'Delete') {
+            str = str.slice(0,-1)
+        } else {
+            str+=element.innerText
+        }
+        console.log(str)
+    })
+});
